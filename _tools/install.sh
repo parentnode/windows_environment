28bc2e6f2135b1f3c554d32dfec719d9c1ad09ce
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-01 22:31:54 +0100

    Working draft

--- _tools/install.sh
+++ _tools/install.sh
@@ -9,161 +9,190 @@ echo "        Starting server installation"
 echo ""
 echo ""
 
-#
-# RedistributableVisualStudio2017="VC_redist.x64.exe"
-# RedistributableVisualStudio2017_path="https://aka.ms/vs/15/release/VC_redist.x64.exe"
-#
-# PHP="php-7.2.2-VC15-x64"
-# PHP_path="http://windows.php.net/downloads/releases/php-7.2.2-Win32-VC15-x64.zip"
-#
-# echo $PHP "instalation with Thread Safe"
-#
-# MariaDB="mariadb-10.2.12-winx64.msi"
-# MariaDB_path="https://downloads.mariadb.org/interstitial/mariadb-10.2.12/winx64-packages/mariadb-10.2.13-winx64.msi?serve"
-#
-# echo $MariaDB "installation version"
-#
-# Apache="apachehttpd-2.4.29-Win64-VC15.zip"
-# Apache_path="http://www.apachelounge.com/download/VC15/binaries/httpd-2.4.29-Win64-VC15.zip"
-#
-# Imagemagick="imagemagick.exe"
-# Imagemagick_path="https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-22-Q16-x64-dll.exe"
-#
-# Ffmpeg="ffmpeg.zip"
-# Ffmpeg_path="https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20180129-d4967c0-win64-static.zip"
-#
-# #cd /mnt/c/srv/sites/parentnode/windows_environment/_tools
-# #./install.sh
-# # Check for the pre
-# if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-#     echo "Windows 10 Bash: OK"
-# else
-#
-#     echo "ERROR: Linux Bash for Windows does not exist"
-#     echo "Install Linux Bash for Windows and try again"
-#     exit 1
-#
-# fi
-#
-# if [ -e /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe ] ; then
-# 	echo "WindowsPowerShell: OK"
-# else
-# 	echo "ERROR: WindowsPowerShell does not exist"
-# 	echo "Install WindowsPowerShell v1.0 or higher and try again"
-# 	exit 1
-# fi
-#
-#
-#
-#
-# if [ -e /mnt/c/ProgramData/chocolatey/bin/choco.exe ] ; then
-# 	echo "Chocolatey: OK"
-# else
-# 	echo "ERROR: Chocolatey does not exist"
-# 	echo "must be installed the chocolatey "
-# 	echo "Installing the chocolatey by executing the executing the folowing script line"
-# 	echo "(Copy and paste in Powershell opened in admin mod) "
-# 	echo "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'"
-#
-# 	echo " Powershell eleveated with administrator privilege"
-# 	echo "(run as admin by click right on the program icon)"
-# 	exit 1
-# fi
-#
-#
-# echo "Checking Directories"
-#
-# if [ -e /mnt/c/srv/sites/parentnode ] ; then
-# 	echo "C:/srv/sites/parentnode alredy exist"
-# else
-# 	echo "create directory C:/srv/sites/parentnode"
-#     mkdir -p /mnt/c/srv/sites/parentnode;
-# fi;
-#
-# if [ -e /mnt/c/srv/tools ] ; then
-# 	echo "C:/srv/tools alredy exist"
-# else
-# 	echo "create directory C:/srv/tools"
-#     mkdir -p /mnt/c/srv/tools;
-# fi;
-#
-# if [ -e /mnt/c/srv/packages ] ; then
-# 	echo "C:/srv/packages alredy exist"
-# else
-# 	echo "create directory C:/srv/packages"
-#     mkdir -p /mnt/c/srv/packages;
-# fi;
+
+vc_compiler="VC_redist.x64.exe"
+vc_compiler_path="https://aka.ms/vs/15/release/VC_redist.x64.exe"
+
+php="php-7.2.2-Win32-VC15-x64.zip"
+php_path="https://phpdev.toolsforresearch.com/php-7.2.2-Win32-VC15-x64.zip"
+
+mariadb="mariadb-10.2.12-winx64.msi"
+mariadb_path="https://downloads.mariadb.org/interstitial/mariadb-10.2.12/winx64-packages/mariadb-10.2.12-winx64.msi?serve"
+
+apache="apachehttpd-2.4.29-Win64-VC15.zip"
+apache_path="https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.29-Win64-VC15.zip"
+
+imagemagick="imagemagick.exe"
+imagemagick_path="https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-22-Q16-x64-dll.exe"
+
+ffmpeg="ffmpeg.zip"
+ffmpeg_path="https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20180129-d4967c0-win64-static.zip"
+
+# Check if windows environment
+if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+    echo "Windows 10 Bash: OK"
+else
+
+    echo "ERROR: Linux Bash for Windows does not exist"
+    echo "Install Linux Bash for Windows and try again"
+    exit 1
+
+fi
+
+echo "Checking Directories"
+
+if [ -e /mnt/c/srv/sites/parentnode ] ; then
+	echo "C:/srv/sites/parentnode already exist"
+else
+	echo "create directory C:/srv/sites/parentnode"
+    mkdir -p /mnt/c/srv/sites/parentnode;
+fi;
+
+if [ -e /mnt/c/srv/packages ] ; then
+	echo "C:/srv/packages already exist"
+else
+	echo "create directory C:/srv/packages"
+    mkdir -p /mnt/c/srv/packages;
+fi;
+
+if [ -e /mnt/c/srv/installed-packages ] ; then
+	echo "C:/srv/installed-packages already exist"
+else
+	echo "create directory C:/srv/installed-packages"
+    mkdir -p /mnt/c/srv/installed-packages;
+fi;
 # if [ -e /mnt/c/srv/packages/ffmpeg ] ; then
-# 	echo "C:/srv/packages alredy exist"
+# 	echo "C:/srv/packages already exist"
 # else
 # 	echo "create directory C:/srv/packages"
 #     mkdir -p /mnt/c/srv/packages/ffmpeg;
 # fi;
 #
-# #/mnt/c/srv/sites/parentnode/windows_environment/_tools
-# echo ""
-# echo "Installing software"
-# echo""
-# hash unzip 2>/dev/null || { echo >&2 "Install: Unzip in Ubuntu enviroment"; sudo apt-get install unzip ;}
-# echo "Skip: unzip in Ubuntu  exist"
-# #sudo apt-get install unzip
-# # hash zip 2>/dev/null || { echo >&2 "Install: zip in Ubuntu enviroment"; sudo apt-get install zip ;}
-# # echo "Skip: zip in Ubuntu  exist"
-# #sudo apt install zip
-#
-# #echo "Installing Chocolatey Server (Simple)"
-# #/mnt/c/ProgramData/chocolatey/bin/choco.exe install -y chocolatey.server
-# #/mnt/c/ProgramData/chocolatey/bin/choco.exe uninstall -y chocolatey.server
-#
-# #echo "Installing nuget.commandline"
-# #/mnt/c/ProgramData/chocolatey/bin/choco.exe uninstall -y nuget.commandline
-# cd /mnt/c/srv/packages/
-#
-# if [ -e /mnt/c/srv/packages/$MariaDB ] ; then
-# 	echo "C:/srv/packages/$MariaDB alredy exist"
-# else
-# 	echo "Downloading: $MariaDB"
-# 	cd /mnt/c/srv/packages/
-# 	wget  -P  /mnt/c/srv/packages/ -O $MariaDB  MariaDB_path
-# 	echo "install mariadb"
-# 	/mnt/c/Windows/SysWOW64/msiexec.exe /i "C:\srv\packages\/"$MariaDB /passive
-# fi;
-#
-# echo "Cechking: for Visual studio 17 C++ exist VC_redist.x64.exe alredy exist"
-#
-# if [ -e /mnt/c/srv/packages/VC_redist.x64.exe ] ; then
-# 	echo "C:/srv/packages/VC_redist.x64.exe alredy exist"
-# else
-# 	cd /mnt/c/srv/packages/
-# 	echo "Download latest C++ Redistributable Visual Studio 2017"
-# 	wget -P  /mnt/c/srv/packages/  -O $RedistributableVisualStudio2017 $RedistributableVisualStudio2017_path
-#
-# 	echo "Installing latest C++ Redistributable Visual Studio 2017"
-# 	/mnt/c/srv/packages/$RedistributableVisualStudio2017
-# fi;
-#
-# echo "Looking: for Apache httpd"
-# if [ -e /mnt/c/srv/packages/$Apache ] ; then
-# 	echo "C:/srv/packages/$Apache alredy exist"
-# else
-# 	cd /mnt/c/srv/packages/
-# 	echo "Downloading: $Apache "
-# 	wget -P  /mnt/c/srv/packages/  -O $Apache Apache_pat
-#
-# 	echo "Extracting: $Apache"
-# 	cd /mnt/c/srv/packages/
-# 	unzip $Apache -d /mnt/c/srv/packages/apache/
-# fi;
-#
-# echo "Looking: for PHP"
-# if [ -e /mnt/c/srv/packages/$PHP ] ; then
-# 	echo "C:/srv/packages/$PHP alredy exist"
-# else
-# 	echo "Downloading:  $PHP VC15 x64 Thread Safe"
-# 	wget -P  /mnt/c/srv/packages/  -O $PHP $PHP_path
-# fi;
+
+echo ""
+echo "Installing software"
+echo ""
+sudo apt-get install unzip
+
+if [ -e /mnt/c/srv/packages/$mariadb ] ; then
+	echo "C:/srv/packages/$mariadb already exist"
+else
+	echo "Downloading: $mariadb"
+	cd /mnt/c/srv/packages/
+#	wget -P /mnt/c/srv/packages -O mariadb-10.2.12-winx64.msi https://downloads.mariadb.org/interstitial/mariadb-10.2.12/winx64-packages/mariadb-10.2.12-winx64.msi?serve
+	wget -O $mariadb $mariadb_path
+
+	echo "installing mariadb"
+    /mnt/c/Windows/SysWOW64/msiexec.exe /i "C:\srv\packages\\"$mariadb /passive
+
+fi;
+
+echo ""
+echo "Checking if $vc_compiler already exist"
+echo ""
+
+if [ -e /mnt/c/srv/packages/$vc_compiler ] ; then
+	echo "C:/srv/packages/$vc_compiler already exist"
+else
+	cd /mnt/c/srv/packages/
+	echo "Downloading c++ compiler"
+	wget -O $vc_compiler $vc_compiler_path
+
+	echo "Installing latest C++ compiler"
+	/mnt/c/srv/packages/$vc_compiler
+fi;
+
+echo "Looking for Apache httpd"
+if [ -e /mnt/c/srv/packages/$apache ] ; then
+	echo "C:/srv/packages/$apache already exist"
+else
+	cd /mnt/c/srv/packages/
+	echo "Downloading: $apache "
+	wget -S -O $apache $apache_path --referer="https://www.apachelounge.com/download/" --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+
+	echo "Extracting: $apache"
+	cd /mnt/c/srv/packages/
+	unzip $apache -d /mnt/c/srv/installed-packages/apache24/
+fi;
+
+echo "Looking for PHP"
+if [ -e /mnt/c/srv/packages/$php ] ; then
+	echo "C:/srv/packages/$php already exist"
+else
+    cd /mnt/c/srv/packages
+	echo "Downloading $php VC15 x64 Thread Safe"
+	wget -O $php $php_path 
+
+    echo "Extracting: $apache"
+	cd /mnt/c/srv/packages/
+	unzip $php -d /mnt/c/srv/installed-packages/php722/
+fi;
+
+
+
+
+echo ""
+echo "Configuring apache server"
+echo ""
+
+sed -i "s/^ServerRoot\ [a-zA-Z0-9\.\_-\"\\:]\+/ServerRoot\ \"C:\\/srv\\/installed-packages\\/apache24\\/Apache24\"/;" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+install_apache_servername=$(grep -E "^ServerName" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf" || echo "")
+
+	if [ -z "$install_apache_servername" ]; then
+
+		# SET SERVERNAME
+		echo "ServerName localhost:80" >> "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+
+	else
+
+		sed -i "s/^ServerName\ [a-zA-Z0-9\.\_-:]\+/ServerName\ localhost:80/;" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+
+	fi
+
+sed -i "s/^DocumentRoot\ [a-zA-Z0-9\.\_-\"\\:]\+/DocumentRoot\ \"C:\\/srv\\/sites\"/;" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+sed -i "s/^<Directory\ \"c:/<Directory\ \"C:\\/srv\\/installed-packages\\/apache24/;" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+install_apache_php=$(grep -E "^LoadModule php7_module" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf" || echo "")
+
+	if [ -z "$install_apache_php" ]; then
+
+		# SET SERVERNAME
+		echo "LoadModule php7_module C:/srv/installed-packages/php722/php7apache2_4.dll" >> "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+	fi
+
+install_apache_php_ini=$(grep -E "^PHPIniDir" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf" || echo "")
+
+	if [ -z "$install_apache_php_ini" ]; then
+
+		# SET SERVERNAME
+		echo "PHPIniDir  C:/srv/installed-packages/php722" >> "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+	fi
+
+cp "/mnt/c/srv/tools/_conf/httpd-vhosts.conf" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/extra/httpd-vhosts.conf"
+install_apache_vhosts=$(grep -E "^Include conf\\/extra\\/httpd\\-vhosts\\.conf" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf" || echo "")
+
+	if [ -z "$install_apache_vhosts" ]; then
+
+		# SET SERVERNAME
+		sed -i "s/^#Include conf\\/extra\\/httpd\\-vhosts\\.conf/Include conf\\/extra\\/httpd\\-vhosts\\.conf/" "/mnt/c/srv/installed-packages/apache24/Apache24/conf/httpd.conf"
+
+	fi
+
+service_installed=$(sudo /mnt/c/Windows/System32/net.exe start apache2.4 exit 2>/dev/null || echo 1)
+if [ "$service_installed" = "1" ]; then 
+sudo /mnt/c/srv/installed-packages/apache24/Apache24/bin/httpd.exe -k install
+fi
+
+sudo /mnt/c/Windows/System32/net.exe start apache2.4
+
 # if [ -e /mnt/c/srv/packages/PHP ] ; then
-# 	echo "C:/srv/packages/PHP alredy exist so $PHP have ben extracted  "
+# 	echo "C:/srv/packages/PHP already exist so $PHP have been extracted  "
 # else
 # 	echo "Extracting: $PHP VC15 x64 Thread Safe zip"
 # 	cd /mnt/c/srv/packages/
@@ -186,7 +215,7 @@ echo ""
 # # /mnt/c/Windows/System32/cmd.exe set PHP=%PHP%; C:\srv\packages\PHP\
 # echo "Looking: for $Ffmpeg"
 # if [ -e /mnt/c/srv/packages/$Ffmpeg ] ; then
-# 		echo "C:/srv/packages/ffmpeg.zip alredy exist but have not ben extracted"
+# 		echo "C:/srv/packages/ffmpeg.zip already exist but have not ben extracted"
 # else
 # 	echo "Downloading: ffmpeg"
 # 	cd /mnt/c/srv/packages/
@@ -194,7 +223,7 @@ echo ""
 # fi;
 #
 # if [ -e /mnt/c/srv/packages/$Ffmpeg ] ; then
-# 	echo "C:/srv/packages/$Ffmpeg alredy exist so have ben extracted"
+# 	echo "C:/srv/packages/$Ffmpeg already exist so have ben extracted"
 # else
 # 	echo "Extracting: $Ffmpeg"
 # 	cd /mnt/c/srv/packages/
@@ -208,7 +237,7 @@ echo ""
 # # echo "Installing ffmpeg"
 # # /mnt/c/srv/packages/ffmpeg.exe
 # if [ -e /mnt/c/srv/packages/$Imagemagick ] ; then
-# 	echo "C:/srv/packages/imagemagick alredy exist"
+# 	echo "C:/srv/packages/imagemagick already exist"
 # else
 #  echo "Downloading: imagemagick"
 #  wget -P  /mnt/c/srv/packages/  -O $Imagemagick $Imagemagick_path
