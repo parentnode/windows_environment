2111069c185a40194af5cb8011498c4c29932dc2
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-08 13:52:08 +0100

    reverting sudo cp

--- _tools/install.sh
+++ _tools/install.sh
@@ -39,8 +39,7 @@ git config --global core.autocrlf input
 
 # Setting up bash config
 echo "Copying .profile to home dir"
-# sudo cp "/mnt/c/srv/tools/_conf/dot_profile" "/home/$SUDO_USER/.profile"
-sudo cp "/mnt/c/srv/tools/_conf/dot_profile" "/home/$(whoami)/.profile"
+ sudo cp "/mnt/c/srv/tools/_conf/dot_profile" "/home/$SUDO_USER/.profile"
 echo ""
 
 # Defining paths and download urls
