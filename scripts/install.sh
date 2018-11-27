e8e4307b57b4f37a189b13cd4ef7e4e90423ab75
David Jessen <jessen-it@outlook.com>
2018-11-27 16:49:26 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -121,10 +121,10 @@ git config --global core.autocrlf true
 #sudo chown "$username:$username" "$HOME/.profile"
 echo "Changed owner"
 
-#check_for_existing_parentnode_dot_profile=$(grep -E "# ADMIN CHECK" "$HOME/.profile")
+check_for_existing_parentnode_dot_profile=$(grep -E "# ADMIN CHECK" "$HOME/.profile" || echo "")
 check_for_existing_alias=$(grep -o "alias" "$HOME/.profile" || echo "")
 echo "Keys Checked"
-if [ -z "$check_for_existing_alias" ];
+if [ -z "$check_for_existing_alias" ] && [ -z "$check_for_existing_parentnode_dot_profile" ];
 then
 	# Setting up bash config
 	echo ""
