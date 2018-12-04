dbf8d25ad799744e642081eefd658ca19ccb3c9a
David Jessen <jessen-it@outlook.com>
2018-12-04 14:28:36 +0100

    Update install_software.sh

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -14,7 +14,10 @@ bash /mnt/c/srv/tools/scripts/install_unzip.sh
 # Downloading and installing c++ compiler
 bash /mnt/c/srv/tools/scripts/install_vc_compiler.sh
 
-# Downloading and installing MariaDB
+# Downloading and installing mariaDB
 bash /mnt/c/srv/tools/scripts/install_mariadb.sh
+
+# Downloading and installing apache
+bash /mnt/c/srv/tools/scripts/install_apache.sh
 
 
