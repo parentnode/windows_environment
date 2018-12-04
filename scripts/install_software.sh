8d6c933c459933a878337f38714de02593f1c7b7
David Jessen <jessen-it@outlook.com>
2018-12-04 14:01:27 +0100

    Splitting up install script

---
+++ scripts/install_software.sh
@@ -0,0 +1,20 @@
+echo ""
+echo "--- Installing software ---"
+echo ""
+
+# Clean up
+echo ""
+echo "Cleaning up:"
+sudo apt-get --assume-yes autoremove
+echo ""
+
+# Install unzip to unpack downloaded packages
+bash /mnt/c/srv/tools/scripts/install_unzip.sh
+
+# Downloading and installing c++ compiler
+bash /mnt/c/srv/tools/scripts/install_vc_compiler.sh
+
+# Downloading and installing MariaDB
+bash /mnt/c/srv/tools/scripts/install_mariadb.sh
+
+
