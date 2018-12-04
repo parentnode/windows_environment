394eb43a984e7873194aa6b52b2b47efadb10bbc
David Jessen <jessen-it@outlook.com>
2018-12-04 14:01:27 +0100

    Splitting up install script

---
+++ scripts/install_mariadb.sh
@@ -0,0 +1,30 @@
+# Setting mariadb name and download link"
+mariadb="mariadb-10-2-12-winx64"
+mariadb_path="https://parentnode.dk/download/72/HTML-uwogdi5x/mariadb-10-2-12-winx64.zip"
+# Old file is also valid (and should not cause re-install)
+mariadb_alt="mariadb-10.2.12-winx64.msi"
+
+# Downloading and installing mariadb
+echo "Looking for $mariadb"
+if [ -e /mnt/c/srv/packages/$mariadb.zip ] || [ -e /mnt/c/srv/packages/$mariadb_alt ] ; then
+	echo "$mariadb already exists"
+else
+
+	echo "Downloading: $mariadb"
+	cd /mnt/c/srv/packages/
+	wget -O $mariadb.zip $mariadb_path
+
+	# Unpack zip
+	unzip $mariadb.zip -d /mnt/c/srv/packages/
+
+	echo ""
+	echo "Installing $mariadb"
+	# Install MariaDB with password and servicename
+	#echo "Dette er mariadb password $db_root_password"
+	sudo /mnt/c/Windows/SysWOW64/msiexec.exe /i "C:\\srv\\packages\\$mariadb.msi" PASSWORD="$db_root_password" SERVICENAME="MariaDB" /qn
+
+	# Remove installer
+	rm /mnt/c/srv/packages/$mariadb.msi
+
+fi
+echo ""
---
+++ scripts/install_unzip.sh
@@ -0,0 +1,9 @@
+# Install unzip to unpack downloaded packages
+echo "Checking unzip:"
+install_unzip=$(unzip || echo "")
+if [ "$install_unzip" = "" ]; then
+	sudo apt-get --assume-yes install unzip
+else
+	echo "unzip is installed"
+fi
+
