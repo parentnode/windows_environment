d2b669a555f0c3e791d4ad1dbed7bc03d508234f
David Jessen <jessen-it@outlook.com>
2018-12-04 15:15:55 +0100

    slimming install.sh

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -6,7 +6,9 @@ apache_path="https://parentnode.dk/download/72/HTML-i59ty49r/apachehttpd-2-4-33-
 apache_service_installed=$(/mnt/c/Windows/System32/sc.exe queryex type= service state= all | grep -E "Apache" || echo "")
 
 # Downloading and installing Apache
+echo ""
 echo "Looking for $apache"
+echo ""
 if [ -e /mnt/c/srv/packages/$apache.zip ] ; then
 	echo "$apache already exists"
 else
@@ -33,21 +35,24 @@ else
 		sudo rm -R /mnt/c/srv/installed-packages/apache24
 	fi
 
-
+	echo ""
 	echo "Downloading: $apache"
+	echo ""
 	cd /mnt/c/srv/packages/
 	wget -O $apache.zip $apache_path
 
 	echo ""
 	echo "Installing $apache"
+	echo ""
 	# Unpack zip to install location
 	unzip $apache.zip -d /mnt/c/srv/installed-packages/apache24
 	
 	# Copy default apache config, before installing service to avoid error
 	cp "/mnt/c/srv/tools/conf/httpd.conf" "/mnt/c/srv/installed-packages/apache24/conf/httpd.conf"
 	sudo /mnt/c/srv/installed-packages/apache24/bin/httpd.exe -k install 
-	echo "apache installed"
-	
 
 fi
 echo ""
+echo "apache installed"
+echo ""
+
--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -9,30 +9,57 @@ sudo apt-get --assume-yes autoremove
 echo ""
 
 # Install unzip to unpack downloaded packages
+echo ""
+echo "Unzip"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_unzip.sh
 
 # Downloading and installing c++ compiler
+echo ""
+echo "C++ Compiler"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_vc_compiler.sh
 
 # Downloading and installing mariaDB
+echo ""
+echo "MariaDB"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_mariadb.sh
 
 # Downloading and installing php
+echo ""
+echo "PHP"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_php.sh
 
 # Downloading and installing apache
+echo ""
+echo "Apache"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_apache.sh
 
 # Downloading and installing imagick
+echo ""
+echo "ImageMagick"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_imagick.sh
 
 # Downloading and installing redis
+echo ""
+echo "Redis"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_redis.sh
 
 # Downloading and installing ffmpeg
+echo ""
+echo "FFMPEG"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_ffmpeg.sh
 
 # Downloading and installing wkhtmltopdf
+echo ""
+echo "WKHTML to PDF"
+echo ""
 bash /mnt/c/srv/tools/scripts/install_wkhtmltopdf.sh
 
 
