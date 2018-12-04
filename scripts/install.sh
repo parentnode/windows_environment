e1d44eb562b7a61db06c0d3f97e8fb77eceeb450
David Jessen <jessen-it@outlook.com>
2018-12-04 12:01:05 +0100

    Trying the normal way

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,8 +18,6 @@ mariadb_alt="mariadb-10.2.12-winx64.msi"
 # Setting apache name and download link"
 apache="apachehttpd-2-4-33-win64-vc15"
 apache_path="https://parentnode.dk/download/72/HTML-i59ty49r/apachehttpd-2-4-33-win64-vc15.zip"
-export apache
-export apache_path
 
 # Setting php name and download link"
 php="php-7-2-2-win32-vc15-x64-redis-4"
@@ -249,7 +247,7 @@ echo ""
 
 # Check if Apache is installed
 apache_service_installed=$(/mnt/c/Windows/System32/sc.exe queryex type= service state= all | grep -E "Apache" || echo "")
-export apache_service_installed
+
 # Check if Apache is running
 apache_service_running=$(/mnt/c/Windows/System32/net.exe start | grep -E "Apache" || echo "")
 # Apache is running (possibly other version)
@@ -361,7 +359,52 @@ fi
 echo ""
 
 
-sudo bash /mnt/c/srv/tools/scripts/install_apache.sh
+# Downloading and installing Apache
+echo "Looking for $apache"
+if [ -e /mnt/c/srv/packages/$apache.zip ] ; then
+	echo "$apache already exists"
+else
+
+	# Uninstall existing service
+	if [ ! -z "$apache_service_installed" ]; then
+
+		echo "APACHE IS RUNNING"
+
+		# Old path
+		if [ -e /mnt/c/srv/installed-packages/apache24/Apache24/bin/httpd.exe ] ; then
+			echo "OLD PATH"
+			sudo /mnt/c/srv/installed-packages/apache24/Apache24/bin/httpd.exe -k uninstall
+		# New path
+		else
+			echo "NEW PATH"
+			sudo /mnt/c/srv/installed-packages/apache24/bin/httpd.exe -k uninstall
+		fi
+
+	fi
+
+	# Remove existing version
+	if [ -e /mnt/c/srv/installed-packages/apache24 ] ; then
+		sudo rm -R /mnt/c/srv/installed-packages/apache24
+	fi
+
+
+	echo "Downloading: $apache"
+	cd /mnt/c/srv/packages/
+	wget -O $apache.zip $apache_path
+
+	echo ""
+	echo "Installing $apache"
+	# Unpack zip to install location
+	unzip $apache.zip -d /mnt/c/srv/installed-packages/apache24
+	
+	# Copy default apache config, before installing service to avoid error
+	cp "/mnt/c/srv/tools/conf/httpd.conf" "/mnt/c/srv/installed-packages/apache24/conf/httpd.conf"
+	sudo /mnt/c/srv/installed-packages/apache24/bin/httpd.exe -k install 
+	echo "apache installed"
+	
+
+fi
+echo ""
 
 
 # Downloading and installing Imagick
