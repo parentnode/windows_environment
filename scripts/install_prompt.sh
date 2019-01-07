5b09d2af4d7b1e1c6214f2bc986730e1a684abb1
david <jessen-it@outlook.com>
2019-01-07 13:50:09 +0100

    Check if prompt needs installing

---
+++ scripts/install_prompt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+if [ "$(checkFileContent "$HOME/.bash_profile" "alias")" == "Found" ];
+then
+    echo "Previous alias statement(s)"
+    if [ "$(checkFileContent "$HOME/.bash_profile" "git_prompt ()")" == "Found" ];
+    then
+        echo ""
+        echo "Seems like you have installed parentnode prompt"
+        echo ""
+    else 
+        echo ""
+        echo "Seems like you haven't installed parentnode prompt"
+        echo ""
+        echo "Installing"
+        copyParentNodepromptToFile
+        echo ""
+    fi
+    #sudo chown "$username:$username" "$HOME/.profile"
+    handleAlias 
+
+else
+
+    if [ "$(checkFileContent "$HOME/.bash_profile" "git_prompt ()")" == "Found" ];
+    then
+        echo "You allready have parentNode Configuration"
+    else 
+        echo "Copying parentNode Configuration"
+        copyParentNodepromptToFile
+
+    fi
+    handleAlias    
+fi
+
