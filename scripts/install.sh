ae253a712a0b7e2dd96e7a4ca264519da1c9f2d1
David Jessen <jessen-it@outlook.com>
2019-01-07 16:05:09 +0100

    refactoring

--- scripts/install.sh
+++ scripts/install.sh
@@ -129,6 +129,7 @@ checkFileContent(){
 }
 export -f checkFileContent
 
+# If an alias from parentnode exists then this script will update it, else it will parse it
 handleAlias(){
     IFS=$'\n'
     read_alias_file=$( < "/mnt/c/srv/tools/conf/dot_profile_alias" )
@@ -168,38 +169,52 @@ fi
 echo ""
 echo "--- Checking Directories ---"
 echo ""
+checkFolderOrCreate(){
+	folderName = $1
+	if [ -e $folderName ];
+	then
+		echo "$folderName already exists"
+	else 
+		echo "Create directory $folderName"
+    	mkdir -p $folderName;
+	fi
 
+}
+
+checkFolderOrCreate "$HOME/Desktop/Bobfolder"
 # Base parentnode project location
-if [ -e /mnt/c/srv/sites/parentnode ] ; then
+if [ -e /mnt/c/srv/sites/parentnode ]; then
 	echo "C:/srv/sites/parentnode already exists"
 else
 	echo "Create directory C:/srv/sites/parentnode"
-    mkdir -p /mnt/c/srv/sites/parentnode;
-fi;
+    mkdir -p /mnt/c/srv/sites/parentnode
+fi
 
 # Base apache configuration location
-if [ -e /mnt/c/srv/sites/apache/logs ] ; then
+if [ -e /mnt/c/srv/sites/apache/logs ]; then
 	echo "C:/srv/sites/apache/logs already exists"
 else
 	echo "Create directory C:/srv/sites/apache/logs"
     mkdir -p /mnt/c/srv/sites/apache/logs;
-fi;
+fi
 
 # Creating packages folder
-if [ -e /mnt/c/srv/packages ] ; then
+if [ -e /mnt/c/srv/packages ]; then
 	echo "C:/srv/packages already exists"
 else
 	echo "Create directory C:/srv/packages"
-    mkdir -p /mnt/c/srv/packages;
-fi;
+    mkdir -p /mnt/c/srv/packages
+fi
 
 # Creating installed-packages folder
-if [ -e /mnt/c/srv/installed-packages ] ; then
+if [ -e /mnt/c/srv/installed-packages ]; then
 	echo "C:/srv/installed-packages already exists"
 else
 	echo "Create directory C:/srv/installed-packages"
-    mkdir -p /mnt/c/srv/installed-packages;
-fi;
+    mkdir -p /mnt/c/srv/installed-packages
+fi
+
+
 echo ""
 
 
