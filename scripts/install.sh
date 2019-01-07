7cda04243f013203eb037cd04ffef7b21bcc7211
David Jessen <jessen-it@outlook.com>
2019-01-07 16:15:40 +0100

    simplify

--- scripts/install.sh
+++ scripts/install.sh
@@ -181,38 +181,43 @@ checkFolderOrCreate(){
 
 }
 
-checkFolderOrCreate "$HOME/Desktop/Bobfolder"
-# Base parentnode project location
-if [ -e /mnt/c/srv/sites/parentnode ]; then
-	echo "C:/srv/sites/parentnode already exists"
-else
-	echo "Create directory C:/srv/sites/parentnode"
-    mkdir -p /mnt/c/srv/sites/parentnode
-fi
+checkFolderOrCreate "/mnt/c/srv/sites/parentnode"
+checkFolderOrCreate "/mnt/c/srv/sites/apache/logs"
+checkFolderOrCreate "/mnt/c/srv/packages"
+checkFolderOrCreate "/mnt/c/srv/installed-packages"
+checkFolderOrCreate "/mnt/c/srv/sites/parentnode"
+#checkFolderOrCreate
+## Base parentnode project location
+#if [ -e /mnt/c/srv/sites/parentnode ]; then
+#	echo "C:/srv/sites/parentnode already exists"
+#else
+#	echo "Create directory C:/srv/sites/parentnode"
+#    mkdir -p /mnt/c/srv/sites/parentnode
+#fi
+#
+## Base apache configuration location
+#if [ -e /mnt/c/srv/sites/apache/logs ]; then
+#	echo "C:/srv/sites/apache/logs already exists"
+#else
+#	echo "Create directory C:/srv/sites/apache/logs"
+#    mkdir -p /mnt/c/srv/sites/apache/logs;
+#fi
 
-# Base apache configuration location
-if [ -e /mnt/c/srv/sites/apache/logs ]; then
-	echo "C:/srv/sites/apache/logs already exists"
-else
-	echo "Create directory C:/srv/sites/apache/logs"
-    mkdir -p /mnt/c/srv/sites/apache/logs;
-fi
-
-# Creating packages folder
-if [ -e /mnt/c/srv/packages ]; then
-	echo "C:/srv/packages already exists"
-else
-	echo "Create directory C:/srv/packages"
-    mkdir -p /mnt/c/srv/packages
-fi
-
-# Creating installed-packages folder
-if [ -e /mnt/c/srv/installed-packages ]; then
-	echo "C:/srv/installed-packages already exists"
-else
-	echo "Create directory C:/srv/installed-packages"
-    mkdir -p /mnt/c/srv/installed-packages
-fi
+## Creating packages folder
+#if [ -e /mnt/c/srv/packages ]; then
+#	echo "C:/srv/packages already exists"
+#else
+#	echo "Create directory C:/srv/packages"
+#    mkdir -p /mnt/c/srv/packages
+#fi
+#
+## Creating installed-packages folder
+#if [ -e /mnt/c/srv/installed-packages ]; then
+#	echo "C:/srv/installed-packages already exists"
+#else
+#	echo "Create directory C:/srv/installed-packages"
+#    mkdir -p /mnt/c/srv/installed-packages
+#fi
 
 
 echo ""
