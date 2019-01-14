1e80bfb55fcaece37b766a7127c207e378acb2e6
david <jessen-it@outlook.com>
2019-01-14 11:49:59 +0100

    forgot to export variables

--- conf/download_name_link.sh
+++ conf/download_name_link.sh
@@ -1,35 +1,49 @@
 # Setting apache name and download link"
 apache="apachehttpd-2-4-33-win64-vc15"
+export apache
 apache_path="https://parentnode.dk/download/72/HTML-i59ty49r/apachehttpd-2-4-33-win64-vc15.zip"
-
+export apache_path
 # Setting ffmpeg name and download link"
 ffmpeg="ffmpeg-20180129-d4967c0-win64"
+export ffmpeg
 ffmpeg_path="https://parentnode.dk/download/72/HTML-knnkg3yn/ffmpeg-20180129-d4967c0-win64.zip"
-
+export ffmpeg_path
 # Getting imagick name and download link"
 imagick="imagemagick-6-9-9-37-q16-x64-dll"
+export imagick
 imagick_path="https://parentnode.dk/download/72/HTML-940u1z9m/imagemagick-6-9-9-37-q16-x64-dll.zip"
-
+export imagick_path
 # Setting mariadb name and download link"
 mariadb="mariadb-10-2-12-winx64"
+export mariadb
 mariadb_path="https://parentnode.dk/download/72/HTML-uwogdi5x/mariadb-10-2-12-winx64.zip"
+export mariadb_path
 # Old file is also valid (and should not cause re-install)
 mariadb_alt="mariadb-10.2.12-winx64.msi"
+export mariadb_alt
 
 # Setting php name and download link"
 php="php-7-2-2-win32-vc15-x64-redis-4"
+export php
 php_path="https://parentnode.dk/download/72/HTML-aqwla8g3/php-7-2-2-win32-vc15-x64-redis-4.zip"
+export php_path
 
 # Setting redis name and download link"
 redis="redis-x64-4-0-2-2"
+export redis
 redis_path="https://parentnode.dk/download/72/HTML-wc8evnh2/redis-x64-4-0-2-2.zip"
+export redis_path
 
 # Setting c++ compiler name and download link"
 vc_compiler="vc_redist-x64"
+export vc_compiler
 vc_compiler_path="https://parentnode.dk/download/72/HTML-ikg9m2me/vc_redist-x64.zip"
+export vc_compiler_path
 # Old file is also valid (and should not cause re-install)
 vc_compiler_alt="VC_redist.x64.exe"
-
+export vc_compiler_alt
 # Setting redis name and download link"
 wkhtmltopdf="wkhtmltopdf-static-0-12-3"
+export wkhtmltopdf
 wkhtmltopdf_path="https://parentnode.dk/download/72/HTML-g2y0tm22/wkhtmltopdf-static-0-12-3.zip"
+export wkhtmltopdf_path
