d883ec2c20efe5c32018f5ca148a5eeda4b16804
David Jessen <jessen-it@outlook.com>
2019-01-18 11:02:57 +0100

    Added comments

--- scripts/install_prompt.sh
+++ scripts/install_prompt.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 
+# First check makes sure we don't override a user's custom alias
 if [ "$(checkFileContent "$HOME/.bash_profile" "alias")" == "Found" ];
 then
     echo "Previous alias statement(s)"
+    # Second tjeck if git_prompt are found
     if [ "$(checkFileContent "$HOME/.bash_profile" "git_prompt ()")" == "Found" ];
     then
         echo ""
         echo "Seems like you have installed parentnode prompt"
+        # Updates git_prompt()
         copyParentNodePromptToFile
         echo ""
     else 
@@ -14,14 +17,16 @@ then
         echo "Seems like you haven't installed parentnode prompt"
         echo ""
         echo "Installing"
+        # Installs git_prompt()
         copyParentNodePromptToFile
         echo ""
     fi
-    #sudo chown "$username:$username" "$HOME/.profile"
+    # Updates parentnode alias
     handleAlias 
 
 else
-
+    # Second tjeck if git_prompt are found for an edge case of deleted alias,
+    # or if only base .bash_profile are created 
     if [ "$(checkFileContent "$HOME/.bash_profile" "git_prompt ()")" == "Found" ];
     then
         echo "You allready have parentNode Configuration"
@@ -31,6 +36,7 @@ else
         copyParentNodePromptToFile
 
     fi
+    # Installs alias
     handleAlias    
 fi
 
