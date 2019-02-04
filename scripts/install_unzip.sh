d2bcc86b74f41dbdef1c86f0448a316f5cb7b889
David Jessen <jessen-it@outlook.com>
2019-02-04 17:13:44 +0100

    Update install_unzip.sh

--- scripts/install_unzip.sh
+++ scripts/install_unzip.sh
@@ -5,10 +5,17 @@ echo "---Checking unzip---"
 echo "--------------------"
 install_unzip=$(unzip 2>/dev/null || echo "")
 if [ "$install_unzip" = "" ]; then
+	echo "------------------------------------"
+	echo "---Unzip not previously installed---"
+	echo "------------------------------------"
+	echo ""
 	echo "----------------------"
 	echo "---Installing unzip---"
 	echo "----------------------"
 	sudo apt-get --assume-yes install unzip
+	echo "------------------------"
+	echo "---unzip is installed---"
+	echo "------------------------"
 else
 	echo "------------------------"
 	echo "---unzip is installed---"
