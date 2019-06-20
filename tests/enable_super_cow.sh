b897f94ddeb2f9038f31004d9dd658ad85ac98c3
jessen1989 <jessen-it@outlook.com>
2019-06-20 14:34:00 +0200

    added enable super cow

--- tests/enable_super_cow.sh
+++ tests/enable_super_cow.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
 source /mnt/c/srv/sites/parentnode/windows_environment/scripts/functions.sh
-username="$(getUsername)"
-echo "Current user is: $username"
+
+echo "Cow power check"
+enableSuperCow
+echo "Check done"
