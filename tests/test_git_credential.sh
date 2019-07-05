25644eaa81a6588e64db166ddb49e885fa472572
jessen1989 <jessen-it@outlook.com>
2019-07-05 12:51:07 +0200

    removed chmod command (to much of a hassle)

--- tests/test_git_credential.sh
+++ tests/test_git_credential.sh
@@ -2,7 +2,7 @@
 source /mnt/c/srv/sites/parentnode/windows_environment/scripts/functions.sh
 # Function checkGitCredential
 # Check if credential are set
-
+echo "Testing testGitCredential"
 #Usage: check if there is value in
 
 #Git.username
