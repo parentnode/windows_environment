28774172d893ed50ad1b34470a40467fb6a1eae1
jessen1989 <jessen-it@outlook.com>
2019-07-09 15:58:37 +0200

    added install mariadb command

---
+++ tests/check_mariadb_password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/mac_environment/scripts/functions.sh
+# Ask the mariadb session if user can log in without password
+# if yes return Y
+#valid_answers=("using password: NO" "using password: YES")
+
+#checkMariadbPassword "/mnt/c/Program\ Files/MariaDB\ 10.2/bin/mysql.exe -uroot" "${valid_status[@]}" 
+
