e090f1716c49bfc83cfd42a1f08643d157e7ce35
jessen1989 <jessen-it@outlook.com>
2020-03-30 12:27:36 +0200

    trying to fix first run apache error withn root access

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -193,7 +193,7 @@ else
 	unzip $apache.zip -d /mnt/c/srv/installed-packages/apache24
 
 	# Copy default apache config, before installing service to avoid error
-	cp "/mnt/c/srv/tools/conf/httpd.conf" "/mnt/c/srv/installed-packages/apache24/conf/httpd.conf"
+	sudo cp "/mnt/c/srv/tools/conf/httpd.conf" "/mnt/c/srv/installed-packages/apache24/conf/httpd.conf"
 
 	# Install service
 	sudo /mnt/c/srv/installed-packages/apache24/bin/httpd.exe -k install
