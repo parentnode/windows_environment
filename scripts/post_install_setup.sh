cb472d49ebda2195d74be640374d48699877e94e
jessen1989 <jessen-it@outlook.com>
2020-04-03 09:33:49 +0200

    clean up test

--- scripts/post_install_setup.sh
+++ scripts/post_install_setup.sh
@@ -1,40 +1,26 @@
 #!/bin/bash -e
 outputHandler "section" "Setting up configuration files for the webserver"
-echo "-----------------------------------------"
-echo "--- Configuring Apache server and PHP ---"
-echo "-----------------------------------------"
+outputHandler "comment"  "Configuring Apache server and PHP"
 
-echo ""
-echo "----------------------------------------------"
-echo "Setting up php.ini and required files for CURL"
-echo "----------------------------------------------"
-echo ""
+outputHandler "comment" "Setting up php.ini and required files for CURL"
 # Setting up php.ini
-echo "Copying php.ini to php722/php.ini"
+outputHandler "comment" "Copying php.ini to php722/php.ini"
 cp "/mnt/c/srv/tools/conf/php.ini" "/mnt/c/srv/installed-packages/php722/php.ini"
-echo ""
+
 
 
 # Setting up php.ini (and required files for CURL)
-echo "Copying libeay32.dll and ssleay32.dll to apache24/bin"
+outputHandler "comment" "Copying libeay32.dll and ssleay32.dll to apache24/bin"
 cp "/mnt/c/srv/installed-packages/php722/libeay32.dll" "/mnt/c/srv/installed-packages/apache24/bin/libeay32.dll"
 cp "/mnt/c/srv/installed-packages/php722/ssleay32.dll" "/mnt/c/srv/installed-packages/apache24/bin/ssleay32.dll"
-echo ""
 
-echo "-----------------------------"
-echo "--- Setting up httpd.conf ---"
-echo "-----------------------------"
-
+outputHandler "comment" "Setting up httpd.conf"
 # Setting up httpd.conf
-echo "Copying httpd config file to apache24/conf"
+outputHandler "comment" "Copying httpd config file to apache24/conf"
 cp "/mnt/c/srv/tools/conf/httpd.conf" "/mnt/c/srv/installed-packages/apache24/conf/httpd.conf"
-echo ""
 
-echo "-----------------------"
-echo "--- Adding SSL cert ---"
-echo "-----------------------"
+outputHandler "comment" "Adding SSL cert"
 
 # Adding SSL cert
-echo "Copying cacert.pem to installed-packages"
+outputHandler "comment" "Copying cacert.pem to installed-packages"
 cp "/mnt/c/srv/tools/conf/cacert.pem" "/mnt/c/srv/installed-packages/cacert.pem"
-echo ""
