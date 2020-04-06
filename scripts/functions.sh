12734fbb1fb90435661d6a635dbbf1f36293118d
jessen1989 <jessen-it@outlook.com>
2020-04-06 09:45:17 +0200

    clean up before pull

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -198,21 +198,7 @@ export -f syncronizeAlias
 
 # Updates all the sections in the .bash_profile file with files in parentnode dot_profile
 createOrModifyBashProfile(){
-	# if $shell_interactive have value, the computer is accessed with an login prompt normally a server
-	conf="/mnt/c/srv/tools/conf/dot_profile"
-	conf_alias="/mnt/c/srv/tools/conf/dot_profile_alias"
-	#install_bash_profile=$(grep -E ". $HOME/.bash_profile" $HOME/.bashrc || echo "")
-	#install_bash_profile=$(grep -E "\$HOME\/\.bash_profile" /home/$install_user/.bashrc || echo "")
-	#if [ -z "$install_bash_profile" ]; then
-	#	outputHandler "comment" "Setting up .bash_profile"
-	#	# Add .bash_profile to .bashrc
-	#	echo "" >> $HOME/.bashrc
-	#	echo "if [ -f \"$HOME/.bash_profile\" ]; then" >> $HOME/.bashrc
-	#	echo " . $HOME/.bash_profile" >> $HOME/.bashrc
-	#	echo "fi" >> $HOME/.bashrc
-	#else
-	#	outputHandler "comment" ".bash_profile Installed"
-	#fi
+
 	if [ "$(fileExists "$HOME/.bash_profile")" = true ]; then
 		outputHandler "comment" ".bash_profile Exist"
 		bash_profile_modify_array=("[Yn]")
